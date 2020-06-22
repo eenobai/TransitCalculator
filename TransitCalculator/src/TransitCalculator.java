76ed573aa96eaeabb9b5716ff5854fa841392123
eenobei <67269852+eenobei@users.noreply.github.com>
2020-06-22 14:36:54 +0300

    Update TransitCalculator.java
    replaced clunky if with more readable if

--- TransitCalculator/src/TransitCalculator.java
+++ TransitCalculator/src/TransitCalculator.java
@@ -14,24 +14,26 @@ class TransitCalculator {
 	
 	public double unlimited7Price() {
 		int numberOf7DayPasses = 1;
+		double modula = numberOfDays % 7;
+		double divis = numberOfDays / 7;
 		
-		if(numberOfDays % 7 != 0 && numberOfDays / 7 != 1 && numberOfDays / 7 > 1 && numberOfDays / 7 < 2) {
+		if(modula != 0 && divis != 1 && numberOfDays / 7 > 1 && numberOfDays / 7 < 2) {
 			numberOf7DayPasses = 2;
-		}else if(numberOfDays % 7 != 0 && numberOfDays / 7 != 2 && numberOfDays / 7 >2 && numberOfDays / 7 < 3){
+		}else if(modula != 0 && divis != 2 && numberOfDays / 7 >2 && numberOfDays / 7 < 3){
 			numberOf7DayPasses = 3;			
-		}else if(numberOfDays % 7 != 0 && numberOfDays / 7 != 3 && numberOfDays / 7 >3 && numberOfDays / 7 < 4){
+		}else if(modula != 0 && divis != 3 && numberOfDays / 7 >3 && numberOfDays / 7 < 4){
 			numberOf7DayPasses = 4;			
-		}else if(numberOfDays % 7 != 0 && numberOfDays / 7 != 4 && numberOfDays / 7 >4 && numberOfDays / 7 < 5){
+		}else if(modula != 0 && divis != 4 && numberOfDays / 7 >4 && numberOfDays / 7 < 5){
 			numberOf7DayPasses = 5;			
-		}else if(numberOfDays % 7 == 0 && numberOfDays / 7 == 1){
+		}else if(modula == 0 && divis == 1){
 			numberOf7DayPasses = 1;			
-		}else if(numberOfDays % 7 == 0 && numberOfDays / 7 == 2){
+		}else if(modula == 0 && divis == 2){
 			numberOf7DayPasses = 2;			
-		}else if(numberOfDays % 7 == 0 && numberOfDays / 7 == 3){
+		}else if(modula == 0 && divis == 3){
 			numberOf7DayPasses = 3;			
-		}else if(numberOfDays % 7 == 0 && numberOfDays / 7 == 4){
+		}else if(modula == 0 && divis == 4){
 			numberOf7DayPasses = 4;			
-		}else if(numberOfDays % 7 == 0 && numberOfDays / 7 == 5){
+		}else if(modula == 0 && divis == 5){
 			numberOf7DayPasses = 5;			
 		}
 		double priceOfOneRide = numberOf7DayPasses * prices[1] / numberOfRides;
@@ -41,7 +43,7 @@ class TransitCalculator {
 	
 	
 	public static void main(String[] args){
-		TransitCalculator testPerson = new TransitCalculator(20, 8);
+		TransitCalculator testPerson = new TransitCalculator(20, 19);
 		System.out.println(testPerson.unlimited7Price());
 	}
 }
